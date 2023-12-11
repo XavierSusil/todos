4898b3fcef508f768a389a7b1ce9dcf5f81258ce
Susil A <xaviersusil@gmail.com>
2023-12-11 19:17:32 +0530

    transitions are updated in create Theme

--- src/customThemes.js
+++ src/customThemes.js
@@ -1,36 +1,43 @@
 import { createTheme, responsiveFontSizes } from "@mui/material";
 
+const baseTheme = createTheme();
 
+const customCreate = (baseCreate) => (property) => {
+  const baseTransition = baseCreate(property);
+
+  return `${baseTransition},transform 0.1s ease-out`;
+};
 let theme = createTheme({
-    palette: {
-        primary: {
-            main: "#4D4FB2"
-        },
-        secondary :{
-            main : "#B2B04D"
-        },
-        green:{
-            main: "#00FF00"
-        },
-        yellow:{
-            main: "#FFFF00"
-        },
-        red: {
-            main: "#FF0000"
-        },
-        white: {
-            main:"#FFFFFF"
-        }
+  palette: {
+    primary: {
+      main: "#4D4FB2",
     },
-    typography :{
-        //fontFamily: "Fira code, Poppins, sans-serif"
-        fontFamily:"Poppins, Roboto, sans-serif"
+    secondary: {
+      main: "#B2B04D",
     },
-    transitions: {
-        create: (props) => `transform 0.1s ease-out`,
-      },
-})
+    green: {
+      main: "#00FF00",
+    },
+    yellow: {
+      main: "#FFFF00",
+    },
+    red: {
+      main: "#FF0000",
+    },
+    white: {
+      main: "#FFFFFF",
+    },
+  },
+  typography: {
+    //fontFamily: "Fira code, Poppins, sans-serif"
+    fontFamily: "Poppins, Roboto, sans-serif",
+  },
+  transitions: {
+    ...baseTheme.transitions,
+    create: customCreate(baseTheme.transitions.create),
+  },
+});
 
-theme = responsiveFontSizes(theme)
+theme = responsiveFontSizes(theme);
 
 export default theme;
